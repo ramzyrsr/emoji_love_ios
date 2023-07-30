d33d802c4665174699759bbe81a6a68565365af4
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2023-07-30 12:28:01 +0700

    intial commit

---
+++ EmojiLove/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  EmojiLove
+//
+//  Created by Ramzy Syahrul R on 30/07/23.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 😀, 🥲, 😡, 👻
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .👻
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {emoji in Text(emoji.rawValue)}
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
