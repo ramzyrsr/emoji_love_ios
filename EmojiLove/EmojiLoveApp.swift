9ed9b445d31eadaa3d86cc0184ac294619f9b438
ramzyrsr <79071247+ramzyrsr@users.noreply.github.com>
2023-07-30 13:01:23 +0700

    initial commit (#1)
    * intial commit
    
    * intial commit
    
    * init commit

---
+++ EmojiLove/EmojiLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoveApp.swift
+//  EmojiLove
+//
+//  Created by Ramzy Syahrul R on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
